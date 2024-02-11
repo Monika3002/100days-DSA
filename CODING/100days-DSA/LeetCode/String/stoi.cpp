7de7d82dce8a397d1740747288be04ac94e13306
Monika3002 <monika25io038@satiengg.in>
2024-02-11 21:52:02 +0530

    done

---
+++ CODING/100days-DSA/LeetCode/String/stoi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int myAtoi(string s) {
+        s.erase(0, s.find_first_not_of(' '));
+        if (s.empty()) return 0;
+
+        int i = 0, num = 0, sign = 1;
+
+        if (s[0] == '+' || s[0] == '-') {
+            sign = (s[0] == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < s.length() && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            if (num > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            num = num * 10 + digit;
+            i++;
+        }
+
+        return sign * num;
+    }
+};
+int main(){
+    Solution s;
+    cout<<s.myAtoi("42");
+    return 0;
+}
